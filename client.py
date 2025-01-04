41ce8e23b4666c90a4ad955f870e569ce5bfe145
nojnis <128159902+nojnis@users.noreply.github.com>
2025-01-04 19:01:35 +0200

    updated client.py
    added some more stuff

--- client.py
+++ client.py
@@ -15,3 +15,53 @@ def send(d):
     sock.send(json.dumps(d).encode())
   except:
     return False
+
+
+# Send server authorization intents, op = 1
+
+send({
+    "op": 0,
+    "d": {
+        "username": input("Enter Username: ")
+    }
+})
+
+# Communicate with server
+
+while True:
+    d = recv()
+    
+    if not d:
+        print("Error recieving math equation from server")
+        break
+    
+    # if op == 1 that means it's a math equation
+
+    if d['op'] == 1:
+        # Loop in the math eqation until it's correct
+        
+        while True:
+                # Print the math equation, the server will send something like x + y
+        
+                answer = input(f"Math equation: {d['d']['sum']} = ")
+                
+                # Send the answer and await a response
+        
+                if not send({"op": 1, "d": {"answer", answer}}):
+                    print("Error submitting answer")
+                    break
+        
+                response = recv()
+        
+                if not response:
+                    print("Error recieving response from server")
+                    break
+                 
+                if response['op'] == 2:
+                    if response['d']['correct']:
+                          print(f"{answer} is correct!")
+                          break
+                    else:
+                          print(f"{answer} is incorrect")
+
+print("Game finished!")
